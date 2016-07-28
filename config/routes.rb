8ad81768b0aa2c55ca81e3bd06a1509b5488ce35
VanThaoNguyen <vanthao1204.cntt@gmail.com>
2016-07-28 11:18:03 +0700

    add user_fatories and posts_controller#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'welcome#index'
+  resources :posts, only: [:index]
+
+  root 'posts#index'
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:first_name) { |n| "Theo#{n}" }
+    last_name             'Nguyen'
+    sequence(:email)      { |n| "theo#{n}@example.com" }
+    password              '123123123'
   end
 end
