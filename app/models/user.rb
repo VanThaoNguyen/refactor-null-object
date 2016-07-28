ad2c415756229aeb7a39e0a5503c68e4b51f0128
VanThaoNguyen <vanthao1204.cntt@gmail.com>
2016-07-28 10:58:08 +0700

    add user_spec#validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name,    presence: true
+  validates :last_name,     presence: true
+
   def name
     return if first_name.blank? or last_name.blank?
     self.name = first_name + ' ' + last_name
